71bc52433be7914d65ab89d054f0d67fe12ce528
materamaba <materamaba@gmail.com>
2026-03-25 12:09:43 +0100

    8. Dodanie adnotacji @Override

--- src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
+++ src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
@@ -7,7 +7,7 @@ public class HanoiRhymer extends DefaultCountingOutRhymer {
     public int reportRejected() {
         return totalRejected;
     }
-
+    @Override
     public void countIn(int in) {
         if (!callCheck() && in > peekaboo())
             totalRejected++;
