735f03f6d0ba355df49ac331a413fc0d30c2abd0
Bartek <xenn01011@gmail.com>
2026-03-30 01:29:34 +0200

    9. zmniejszenie widocznosci atrybutow klas

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -8,7 +8,7 @@ public class DefaultCountingOutRhymer {
 
     private final int[] numbers = new int[CAPACITY];
 
-    public int total = EMPTY;
+    private int total = EMPTY;
 
     public void countIn(int in) {
         if (!isFull())
